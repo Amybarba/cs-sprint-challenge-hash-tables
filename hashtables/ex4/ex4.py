0273c4efd5cf00910d0f9f9ec48918871a0138da
Amy Barba <Amybarba70@gmail.com>
2020-11-09 19:35:34 -0800

    example 4 complete and passed

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,9 +3,19 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    hash_table = {}
+    result = []
 
+    for num in a:
+        abs_val = abs(num)
+
+        if hash_table.get(abs_val) is not None:
+            result.append(abs_val)
+        else:
+            hash_table[abs_val] = abs_val
     return result
 
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+
